3cd53d3555b97ba5a8cab2095b433c8c86e2e061
Admin <rmilan771@gmail.com>
2020-08-13 09:25:13 +0545

    gallery page added by Kirawa

---
+++ lib/Pages/Gallery_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GalleryPage extends StatefulWidget {
+  @override
+  _GalleryPageState createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("This is awesome Gallery page"),
+      ),
+    );
+  }
+}
